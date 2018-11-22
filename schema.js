1043ee7db485ae78eff9772088d932bd23207600
Aaron Adams <aaron@formspoint.com>
2018-11-21 21:07:32 -0600

    added json-server & axios for fetching data

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -8,11 +10,11 @@ const {
 } = require('graphql');
 
 // TEMP: Hardcoded Data
-const customers = [
-  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
-  { id: '1', name: 'Steve Smith', email: 'ssmith@gmail.com', age: 25 },
-  { id: '1', name: 'Sarah Same', email: 'ssame@gmail.com', age: 40 },
-];
+// const customers = [
+//   { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+//   { id: '2', name: 'Steve Smith', email: 'ssmith@gmail.com', age: 25 },
+//   { id: '3', name: 'Sarah Same', email: 'ssame@gmail.com', age: 40 },
+// ];
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -35,17 +37,21 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString },
       },
       resolve(parentValue, args) {
-        for(let i = 0; i < customers.length; i++) {
-          if(customers[i].id === args.id){
-            return customers[i];
-          }
-        }
+        // for(let i = 0; i < customers.length; i++) {
+        //   if(customers[i].id === args.id){
+        //     return customers[i];
+        //   }
+        // }
+        return axios.get(`http://localhost:3000/customers/${args.id}`)
+          .then(res => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        // return customers;
+        return axios.get(`http://localhost:3000/customers`)
+          .then(res => res.data);
       }
     }
   }
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema.js');
 
+// import * as express from 'express';
+// import expressGraphQL from 'express-graphql';
+// import schema from './schema.js';
+
 const app = express();
 
 app.use('/graphql', expressGraphQL({
